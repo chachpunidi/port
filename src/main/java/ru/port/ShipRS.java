2d4dc6716b6535dbe1c7cf64d3f95f4204a0477b
Alehandr <chachpunidi@gmail.com>
2021-05-17 21:09:56 +0300

    Задание 3. Готово

--- src/main/java/ru/port/ShipRS.java
+++ src/main/java/ru/port/ShipRS.java
@@ -4,6 +4,7 @@ import ru.port.model.ShipDto;
 import ru.port.service.InMemoryShipServiceImpl;
 import ru.port.service.ShipService;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/ships/")     // аннотация из jax-rs привязывает все методы класса к адерсу {address-to-services}/aircrafts
 @Produces(MediaType.APPLICATION_JSON) // аннотация из jax-rs говорит во что преобразововать отдаваемые java объекты.
+@Singleton // Чтоб каждый раз не создавался новый инстанс ShipRS. Переиспользуем инстанс который создался при первом обращении
 public class ShipRS {
 
     private ShipService shipService = new InMemoryShipServiceImpl();
--- src/main/java/ru/port/service/InMemoryShipServiceImpl.java
+++ src/main/java/ru/port/service/InMemoryShipServiceImpl.java
@@ -2,16 +2,31 @@ package ru.port.service;
 
 import ru.port.model.ShipDto;
 
-public class InMemoryShipServiceImpl implements ShipService{
+import java.util.ArrayList;
+import java.util.List;
 
-    public ShipDto getShipByCode(String shipCode) {
-        ShipDto ship =new ShipDto();
-        ship.setName("hh");
-        ship.setModel("kk");
-        ship.setShipsCode(shipCode);
-        return ship;
+public class InMemoryShipServiceImpl implements ShipService{
+    List<ShipDto> ships;
+
+    public  InMemoryShipServiceImpl() {
+        ships = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            ShipDto ship = new ShipDto();
+            ship.setName("hh");
+            ship.setModel("kk");
+            ship.setShipsCode(i + "");
+
+            ships.add(ship);
+        }
     }
 
-
-
+    public ShipDto getShipByCode(String shipCode) {
+        for (ShipDto ship : ships) {
+            if (ship.getShipsCode().equals(shipCode)) {
+                return ship;
+            }
+        }
+        return null;
+    }
 }
